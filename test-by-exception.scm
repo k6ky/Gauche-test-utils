fc86ca3b95ce380cfe68a2c4f18f4b1df1587cab
kyosuke kawai <askjkawai@gmail.com>
2014-09-24 11:42:59 +0900

    init

---
+++ test-by-exception.scm
@@ -0,0 +1,36 @@
+(define-module test-utils.test-by-exception
+  (use gauche.test)
+  (export test-by-ex*
+          coin-test*))
+(select-module test-utils.test-by-exception)
+
+(define (trying number thunk)
+  (/ (fold (lambda (_ acc)
+             (+ acc (thunk)))
+           0
+           (iota number))
+     number))
+(define (in-pardon? exception pardon value)
+  (if (and (> value (* exception (- 1 pardon)))
+           (< value (* exception (+ 1 pardon))))
+      #t
+       (error "the value is not in ragne" (+ 0.0 value))))
+(define-syntax test-by-ex*
+  (syntax-rules ()
+    ((_ test-name exception trying-number pardon expr)
+     (test* (string-append "RANDOM: " test-name)
+            #t
+             (in-pardon? exception
+                         pardon
+                         (trying trying-number (lambda () expr)))))))
+(define-syntax coin-test*
+  (syntax-rules ()
+    ((_ test-name trying-number pardon succuess-rate fail-rate expr)
+     (test-by-ex* test-name
+                  (/ succuess-rate (+ succuess-rate fail-rate))
+                  trying-number
+                  pardon
+                  (if expr 1 0)))))
+
+
+(provide "test-by-exception")
---
+++ test-values.scm
@@ -0,0 +1,59 @@
+(define-module test-utils.test-values
+  (extend gauche.test)
+  (use gauche.sequence)
+  (export test-values
+          test-values*))
+(select-module test-utils.test-values)
+
+(define (prim-test-values msg expects thunk)
+  (flush)
+  (receive vals (thunk)
+    (if (= (length vals) (length expects))
+        (for-each-with-index
+         (lambda (i r expect)
+           (test-count++)
+           (let ((cmp test-check)
+                                        ;(expect (if (pair? expect) (car expect) expect))
+                                        ;(cmp (if (pair? expect) (cadr expect) test-check))
+                 )
+             (format/ss #t "test ~a(value ~a), expects ~s ==> " msg i expect)
+             (cond [(cmp expect r)
+                    (format #t "ok\n")
+                    (test-pass++)]
+                   [else
+                    (format/ss #t "ERROR: GOT ~S\n" r)
+                    (set! *discrepancy-list*
+                          (cons (list msg expect r) *discrepancy-list*))
+                    (test-fail++)])))
+         vals expects)
+        (for-each (lambda (_)
+                    (test-count++)
+                    (format/ss #t "ERROR: GOT ~S\n" vals)
+                    (set! *discrepancy-list*
+                          (cons (list msg expects vals) *discrepancy-list*))
+                    (test-fail++))
+                  expects))
+    (flush)))
+
+(define (test-values msg expects thunk)
+  (prim-test-values
+   msg expects
+   (lambda ()
+     (guard (e [else
+                (when *test-report-error*
+                  (report-error e))
+                (make <test-error>
+                  :class (class-of e)
+                   :message (if (is-a? e <message-condition>)
+                                (ref e 'message)
+                                e))])
+       (thunk)))))
+
+(define-syntax test-values*
+  (syntax-rules ()
+    ((_ msg (expects ...) form)
+     (test-values msg (list expects ...) (lambda () form)))
+    ((_ msg expects form)
+     (test-values msg expects (lambda () form)))))
+
+(provide "test-values")
