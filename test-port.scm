244a0cd648325e2acb33980ed5db5b6b3ff8d3cc
kyosuke kawai <askjkawai@gmail.com>
2014-11-20 12:05:22 +0900

    add test-port diff

--- test-port.scm
+++ test-port.scm
@@ -1,25 +1,44 @@
 (define-module test-utils.test-port
-  (use gauche.test)
+  (extend gauche.test)
+  (use text.diff)
   (export test-port))
 (select-module test-utils.test-port)
 
-(define (test-port msg expect proc)
-  (test msg expect
-        (call-with-output-port proc)))
-;; (define-syntax test-port*
-;;   (syntax-rules (port)
-;;     ((_ msg expect form)
-;;      (test-port msg expect ))))
+(define (test-port-writer element type)
+  (case type
+    ((-)
+     (format/ss #t "expect: ~a\n" element))
+    ((+)
+     (format/ss #t "got:    ~a\n" element))
+    ((#f)
+     (format/ss #t "        ~a\n" element))))
 
+(define (prim-test-port msg expect thunk)
+  (flush)
+  (test-count++)
+  (let ((r (thunk))
+        (cmp test-check))
+    (cond ((cmp expect r)
+           (test-pass++)
+           (format/ss #t "test ~a, expects ~s ==> ok\n"
+                      msg expect))
+          (else
+           (test-fail++)
+           (format/ss #t "test ~a ==> Error\n" msg)
+           (diff-report expect r
+                        :writer test-port-writer)))))
+
+(define (test-port msg expect proc)
+  (prim-test-port msg expect
+                  (lambda ()
+                    (guard (e [else
+                               (when *test-report-error*
+                                 (report-error e))
+                               (make <test-error>
+                                 :class (class-of e)
+                                 :message (if (is-a? e <message-condition>)
+                                              (ref e 'message)
+                                              e))])
+                      (call-with-output-string proc)))))
 
 (provide "test-utils.test-port")
-
-
-
-
-
-
-
-
-
-
